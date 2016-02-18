5cd4f8db4e2114d9b2eabfe58311ae970f2ccf81
Chad Duffey <chadduffey@hotmail.com>
2016-02-18 15:59:12 -0800

    project specific stuff started

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+import base64
+import os
+import requests
+import urllib
+
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
@@ -10,31 +15,54 @@ from datetime import datetime
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123SECRETi'
+app.config['DEBUG'] = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(Form):
-	name = StringField('Enter your name', validators=[Required()])
-	submit = SubmitField('Submit')
+#Dropbox App
+APP_KEY = 'uukptwu0r6kc0ok'
+APP_SECRET = 'bctri5jlrgj0srj'
+csrf_token = base64.urlsafe_b64encode(os.urandom(18))
 
-@app.route('/', methods=['GET', 'POST'])
+class AuthDBForm(Form):
+	submit = SubmitField('Authorize Dropbox')
+
+
+@app.route('/')
 def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash("Name updated")
-		session['name'] = form.name.data
-		form.name.data = ''
-		return redirect(url_for('index'))
-	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+	return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
+@app.route('/main', methods=['GET', 'POST'])
+def main(): 	
+	session['csrf_token'] = csrf_token
+	form = AuthDBForm()
+	if form.validate_on_submit():
+		return redirect('https://www.dropbox.com/1/oauth2/authorize?%s' % urllib.urlencode({
+			'client_id': APP_KEY,
+			'redirect_uri': url_for('db_auth_finish', _external=True),
+			'response_type': 'code',
+			'state': csrf_token
+			}))
+	return render_template('main.html', form=form)
+
+
+@app.route('/db_auth_finish', methods=['GET', 'POST'])
+def db_auth_finish():
+	if request.args['state'] != session.pop('csrf_token'):
+		abort(403)
+	data = requests.post('https://api.dropbox.com/1/oauth2/token',
+			data={
+			'code': request.args['code'],
+			'grant_type': 'authorization_code',
+			'redirect_uri': url_for('db_auth_finish', _external=True)},
+			auth=(APP_KEY, APP_SECRET)).json()
+	token = data['access_token']
+	return render_template('main.html', db_auth=True, token=token)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -46,3 +74,4 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
 	manager.run()
+
